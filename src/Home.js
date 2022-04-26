c9985ec6e6d69bb165af7101588fd81aa7a1a166
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-04-26 18:36:39 +0200

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./Bloglist";
 
 const Home = () => {
 
+  // useEffect test
+  useEffect(() => {
+    console.log("use effect ran")
+    console.log(blogs);
+  },[]);
+
+  // useState test
   const [blogs, setBlogs] = useState([
     {title: "my website1", body: "lorem ipsum...", author: "Helen", id: 1 },
     {title: "my website2", body: "lorem ipsum...", author: "Julia", id: 2 },
@@ -10,6 +17,11 @@ const Home = () => {
 
   const [title, setTitle] = useState("button tests");
 
+  const modifyValueOnClick = () => {
+    setTitle("BUTTON TESTS");
+  }
+
+  // function onClick test
   const handleClick = event => {
     console.log("hello", event);
   }
@@ -17,10 +29,14 @@ const Home = () => {
     console.log("hello " + name, e.target);
   }
 
-  const modifyValueOnClick = () => {
-    setTitle("BUTTON TESTS");
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog)=> {
+      return blog.id !== id;
+    })
+    setBlogs(newBlogs);
   }
 
+    // Datas displayed on homepage using functions & props
   return (
     <div className="home">
       <h2> {title} </h2>
@@ -29,7 +45,7 @@ const Home = () => {
       <button onClick={modifyValueOnClick}>Upcase title</button>
 
       <div className="homeList">
-        <BlogList blogs={blogs} title="All blogs"/>
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
         <BlogList blogs={blogs.filter((blog) => blog.author === "Julia")} title="Julia's blog"/>
       </div>
     </div>
