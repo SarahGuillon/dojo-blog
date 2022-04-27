340774b1c4cec43d5d4450ece4f8f8af6970f8ca
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-04-27 12:35:36 +0200

    error message

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,18 @@ import BlogList from "./Bloglist";
 
 const Home = () => {
 
-  // useState test (initial value of blogs => null & we will update it later with fetch)
+  // useState exercize (initial value of blogs => null & we will update it later with fetch)
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
   const [title, setTitle] = useState("button tests");
+  const [error, setError] = useState(null)
 
-  const modifyValueOnClick = () => {
-    setTitle("BUTTON TESTS");
-  }
 
-  // function onClick test
+  // function onClick exercize
   const handleClick = event => {
     console.log("hello", event);
   }
+
   const handleClickAgain = (name, e) => {
     console.log("hello " + name, e.target);
   }
@@ -27,18 +26,39 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
-    // useEffect test
-    useEffect(() => {
-      console.log("use effect ran")
-      fetch('http://localhost:8000/blogs')
-      .then(response => response.json())
+  const modifyValueOnClick = () => {
+    setTitle("BUTTON TESTS");
+  }
+
+  // useEffect exercize with fetch (& with setTimeOut to see the loading message)
+  useEffect(() => {
+    console.log("use effect ran")
+
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogss')
+      .then(response => {
+        console.log(response);
+        // error message if i can access the server but problem with the fetch
+        if (!response.ok) {
+          throw Error("Cannot fetch the data for that resource")
+        }
+        return response.json()})
       .then(data => {
         console.log(data);
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
       })
-    },[]);
+      // error message if it can't connect to the server to fetch("failed to fetch") but doesn't see the error of the API if it can access the server
+      .catch(err => {
+        setError(err.message);
+        setIsPending(false);
+      })
+    }, 1000)
 
-    // Datas displayed on homepage using functions & props
+  },[]);
+
+  // what we displayed on homepage
   return (
     <div className="home">
       <h2> {title} </h2>
@@ -47,8 +67,10 @@ const Home = () => {
       <button onClick={modifyValueOnClick}>Upcase title</button>
 
       <div className="homeList">
-        {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
-        {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Lucie")} title="Lucie's blog"/>}
+        { isPending && <p>Loading...</p> }
+        { error && <div>{ error }</div> }
+        { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/> }
+        { blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Lucie")} title="Lucie's blog"/> }
       </div>
     </div>
   );
